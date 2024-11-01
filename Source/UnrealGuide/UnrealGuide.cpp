f6038d26745ca20aae2b921338b1a35246d01869
1716285375 <1716285375@qq.com>
2024-11-01 09:35:24 +0800

    initialization the project

---
+++ Source/UnrealGuide/UnrealGuide.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealGuide.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealGuide, "UnrealGuide" );
